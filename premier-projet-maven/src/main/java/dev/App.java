dbceb00cc039da50988cb8f37a9ff96e5ed45dbf
Felix <felix.gaillard51@gmail.com>
2021-11-02 15:44:04 +0100

    fin tp

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,13 +1,16 @@
 package dev;
 
+import java.io.IOException;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws IOException {
+		String asciiArt1 = FigletFont.convertOneLine("App");
+		System.out.println(asciiArt1);
+	}
 }
